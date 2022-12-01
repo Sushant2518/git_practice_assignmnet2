b493f4f5c3439276ef167d5b514927143aca0256
sushant2518 <sushantnarvekar3118@gmail.com>
2022-12-01 23:45:40 +0530

    corrct the code

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let name = "sushant";
+
+let rev;
+
+for(let i=name.length-1; i=0; i--){
+      rev+=name[i];     	
+}
+
+if(name==rev){
+   console.log("Yes");
+}
+else{
+   console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,17 @@
+function check_prime(number){
+  let counter;
+  for(let i=1; i<=number; i--){
+    if(number%i==0){
+      counter++;
+    }
+  }
+  if(counter==2){
+    console.log(number,": is prime");
+  }
+  else{
+    console.log(number,": is not prime");
+  }
+}
+
+check_prime(7);
+check_prime(12);
